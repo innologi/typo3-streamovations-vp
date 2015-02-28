2b76e3484146550c4203e9c3f64e0c7672a72260
Frenck Lutke <git@innologi.nl>
2015-02-28 10:43:36 +0100

    [TASK] Expand MagicResponse for completeness
    In preparation of creating a responseMapper solution, we need the
    MagicResponse to be a full alternative. At this point, every
    multi-dimensional array is returned as just that: an array. But when we
    move to a full reponse mapper with actual domain models, arrays need to
    be objectStorages and such, for consistency with other types of domain
    models. The effect is that treating them as an array as you would with
    MagicResponse, will completely fail with the response mapper enabled,
    thus leaving the 2 methods un-interchangable.
    
    A way to solve this, is to make MagicResponse mimic a domain model more
    accurately. When returning a value that is an array, we could do an
    attempt of determining whether this should have been an object (simply
    returned as a new MagicResponse), or an objectStorage containing
    MagicResponses.
    
    When this logic is added to MagicResponse, we're left with one more
    issue: what if a property of a response is to be treated as an array,
    according to variable annotations as defined in the domain model? If the
    array is returned as an objectStorage, this isn't that big of deal,
    because an objectStorage implements several PHP interfaces that allow it
    to be treated as an array. Countable, Iterator and ArrayAccess come to
    mind. Suppose MagicResponse _also_ implements these interfaces, then any
    value orignally an array but returned as an object, could still be
    approached as an array in the most important cases!
    
    Change-Id: I60c0fd6ef42ea060541aca9331f95d78aff18b8f

--- Classes/Library/RestRepository/MagicResponse.php
+++ Classes/Library/RestRepository/MagicResponse.php
@@ -24,6 +24,7 @@ namespace Innologi\StreamovationsVp\Library\RestRepository;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 use TYPO3\CMS\Extbase\Persistence\Generic\Exception\UnsupportedMethodException;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 /**
  * REST Magic Response
  *
@@ -38,16 +39,32 @@ use TYPO3\CMS\Extbase\Persistence\Generic\Exception\UnsupportedMethodException;
  * having to create dozens of Domain Objects just for this
  * purpose, and possibly having to throw them away later on.
  *
- * The reason this is not meant for production-ready extensions,
- * is performance and obvious lack of descriptive domain models.
- * It's a hack that goes against the DDD-principle.
+ * When a property is an array, the instance attempts to find out
+ * if it should return an ObjectStorage or a MagicResponse object.
+ * It will not return an array, ever. Because it cannot reliably
+ * determine automagically whether the use-case expects an object
+ * or an array, it implements several interfaces so that a
+ * MagicResponse-object can be treated as an array instead.
+ *
+ * Although this class is a hack that goes against DDD-principles,
+ * it might benefit performance in some specific use-cases.
+ * Regardless, you should _always_ provide a mappable alternative
+ * with descriptive domain models for production!
  *
  * @package InnologiLibs
  * @subpackage RestRepository
  * @author Frenck Lutke
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
-class MagicResponse implements ResponseInterface {
+class MagicResponse implements ResponseInterface, \ArrayAccess, \Iterator, \Countable {
+
+	/**
+	 * An array representation of all the properties, necessary
+	 * for some of the interfaces to work as expected.
+	 *
+	 * @var array
+	 */
+	protected $__properties = array();
 
 	/**
 	 * Constructor
@@ -58,9 +75,11 @@ class MagicResponse implements ResponseInterface {
 	 * @return void
 	 */
 	public function __construct(array $properties) {
+		$this->__properties = $properties;
 		foreach ($properties as $property => $value) {
 			$this->$property = $value;
 		}
+
 	}
 
 	/**
@@ -74,16 +93,21 @@ class MagicResponse implements ResponseInterface {
 	 */
 	public function __call($methodName, $arguments) {
 		if (substr($methodName, 0, 3) === 'get' && strlen($methodName) > 4) {
-			$propertyName = lcfirst(substr($methodName, 3));
-			return isset($this->$propertyName)
-				? $this->$propertyName
-				: NULL;
+			$property = NULL;
+			$propertyName = $this->getPropertyName($methodName);
+			if (isset($this->$propertyName)) {
+				$property = $this->$propertyName;
+				if (is_array($property)) {
+					$property = $this->processArrayProperty($property);
+				}
+			}
+			return $property;
 		} elseif (substr($methodName, 0, 3) === 'set' && strlen($methodName) > 4) {
 			if (!isset($arguments[0])) {
 				throw new \Exception('Calling ' . $methodName . ' without an argument');
 			}
-			$propertyName = lcfirst(substr($methodName, 3));
-			$this->$propertyName = $arguments[0];
+			$propertyName = $this->getPropertyName($methodName);
+			$this->offsetSet($propertyName, $arguments[0]);
 			return $this;
 		}
 		throw new UnsupportedMethodException('The method "' . $methodName . '" is not supported');
@@ -97,4 +121,101 @@ class MagicResponse implements ResponseInterface {
 	public function __toString() {
 		return serialize($this);
 	}
+
+	/**
+	 * Returns the property name derived from $methodName
+	 *
+	 * @param string $methodName
+	 * @return string
+	 */
+	protected function getPropertyName($methodName) {
+		return lcfirst(substr($methodName, 3));
+	}
+
+	/**
+	 * Processes an array property by determining whether it should be
+	 * an objectStorage or an object.
+	 *
+	 * @param array $array
+	 * @return mixed
+	 */
+	protected function processArrayProperty(array $array) {
+		// if $property is an array it can mean 1 of 3 things
+		$property = current($array);
+		$index = key($array);
+		if (is_array($property) && is_int($index)) {
+			// most likely, this is a list of objects, thus objectStorage
+			$property = $this->createObjectStorage($array);
+		} else {
+			// either $property represents a value of an object or of a simple array
+			// in which case we return a MagicResponse as it can be treated as both
+			$property = new MagicResponse($array);
+		}
+
+		return $property;
+	}
+
+	/**
+	 * Creates an objectStorage of MagicResponse objects out of an array-list
+	 *
+	 * @param array $listOfArrays
+	 * @return \TYPO3\CMS\Extbase\Persistence\ObjectStorage
+	 */
+	protected function createObjectStorage(array $listOfArrays) {
+		$storage = new ObjectStorage();
+		foreach ($listOfArrays as $properties) {
+			$storage->attach(
+				new MagicResponse($properties)
+			);
+		}
+		return $storage;
+	}
+
+
+
+	/*
+	 * The following methods are implementation classes of
+	 * array-related interface classes. I cba to describe them,
+	 * see the interfaces instead.
+	 *
+	 * Note that here, we don't check for array values.
+	 * If you treat the object as an array, arrays don't get
+	 * converted to MagicResponse objects.
+	 */
+
+	public function offsetExists($offset) {
+		return isset($this->__properties[$offset]);
+	}
+	public function offsetGet($offset) {
+		return $this->__properties[$offset];
+	}
+	public function offsetSet($offset, $value) {
+		$this->$offset = $value;
+		$this->__properties[$offset] = $value;
+	}
+	public function offsetUnset($offset) {
+		unset($this->$offset);
+		unset($this->__properties[$offset]);
+	}
+
+	public function current() {
+		return current($this->__properties);
+	}
+	public function next() {
+		return next($this->__properties);
+	}
+	public function key() {
+		return key($this->__properties);
+	}
+	public function valid() {
+		return current($this->__properties) !== FALSE;
+	}
+	public function rewind() {
+		return reset($this->__properties);
+	}
+
+	public function count() {
+		return count($this->__properties);
+	}
+
 }
