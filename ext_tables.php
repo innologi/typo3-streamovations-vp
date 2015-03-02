cc2a8649958a23afd1e56d40eef4d884c83d4ee5
Frenck Lutke <git@innologi.nl>
2015-03-02 15:59:17 +0100

    [TASK] Add Flexform CSH
    Change-Id: Ica9263f1db4aa52ca787263d21f1002d6281e97f

--- ext_tables.php
+++ ext_tables.php
@@ -15,15 +15,16 @@ if (!defined('TYPO3_MODE')) {
 	'Streamovations Video Player'
 );
 
+// add plugin flexform
 $pluginSignature = str_replace('_','',$_EXTKEY) . '_video';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
 	$pluginSignature,
 	'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_video.xml'
 );
-// @TODO add CSH for flexform
-#\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-#	'tt_content.pi_flexform.'.$pluginSignature.'.list',
-#	'EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_csh_flexform_video.xml'
-#);
-// AND/OR? in flexform add <cshFile>LLL:EXT:<extkey>/Resources/Private/Language/locallang_csh_flexform_video.xml</cshFile>
+
+// add plugin flexform csh
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+	'tt_content.pi_flexform.'.$pluginSignature.'.list',
+	'EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_csh_flexform_video.xml'
+);
