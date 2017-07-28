8cdfbfd7a21e6919ebf20dc2faefcc4ae01286f2
Frenck Lutke <git@innologi.nl>
2017-07-28 19:14:34 +0200

    [TASK] Move TCA related stuff out of ext_tables.php
    These now reside in Configuration/TCA/Overrides files
    
    Change-Id: Ie1fdb0141c9206040573cee7a52625cd8e0185be

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,8 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+	'streamovations_vp',
+	'Configuration/TypoScript',
+	'Streamovations Video Player'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -1,30 +1,8 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Video',
-	'Streamovations Video Player'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-	$_EXTKEY,
-	'Configuration/TypoScript',
-	'Streamovations Video Player'
-);
-
-// add plugin flexform
-$pluginSignature = str_replace('_','',$_EXTKEY) . '_video';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-	$pluginSignature,
-	'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_video.xml'
-);
+defined('TYPO3_MODE') or die();
 
 // add plugin flexform csh
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-	'tt_content.pi_flexform.'.$pluginSignature.'.list',
+	'tt_content.pi_flexform.streamovationsvp_video.list',
 	'EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_csh_flexform_video.xml'
 );
