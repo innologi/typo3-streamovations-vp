65f6a44e8f5067b17f95284156d480a0245473d4
Frenck Lutke <git@innologi.nl>
2019-06-20 17:03:38 +0200

    [TASK] Raise TYPO3 compatibility to v9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.8',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.0-8.7.99',
+			'typo3' => '8.7.0-9.5.99',
 			'php' => '7.1'
 		),
 		'conflicts' => array(
